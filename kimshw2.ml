44250ed66d6a12f946ab4cb0a6ab239c237a6089
kimsvatos <kimsvatos@ucla.edu>
2017-01-30 13:26:25 -0800

    syntax error variable order

--- kimshw2.ml
+++ kimshw2.ml
@@ -76,7 +76,7 @@ let rec matchRuleTerm ruleFunc ruleSymbol acceptor frag deriv =
 (* this is where we check arrowList ---- all possible things that symbol could lead to. 
 eg. Expr -> term binop expr       // arrowList is "term binop expr"*)
 and matchRuleList symbol ruleFunc acceptor frag deriv = 
-let arrowList = ruleFunc symbol
+let arrowList = ruleFunc symbol in 
 match arrowList with
 	(*if we've exhausted all possible rule groups, return None*)
 	[] -> None
