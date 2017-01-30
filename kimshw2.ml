d84769519d1411ffc85e57b8d82a98a39928a3a9
kimsvatos <kimsvatos@ucla.edu>
2017-01-30 14:18:29 -0800

    missing parenth

--- kimshw2.ml
+++ kimshw2.ml
@@ -27,16 +27,16 @@ type ('nonterminal, 'terminal) symbol =
 
 
 
-let rec matchRuleTerm ruleFunc ruleSymbol acceptor deriv frag = 
+let rec matchRuleTerm ruleFunc ruleSymbol acceptor frag deriv = 
 	match ruleSymbol with
 	[] -> acceptor deriv frag
 	| _ -> match frag with 
 			| [] -> None
 			| (fragHead)::(fragTail) -> match ruleSymbol with 
 						| (N nonTermHead)::(nonTermTail) -> 
-						(matchRuleList (nonTermHead) (ruleFunc) (ruleFunc nonTermHead) (matchRuleTerm ruleFunc nonTermTail acceptor) deriv frag)
+						(matchRuleList (nonTermHead) (ruleFunc) (ruleFunc nonTermHead) (matchRuleTerm ruleFunc nonTermTail acceptor) frag deriv)
 				     	| (T termHead)::(termTail) -> (if fragHead = termHead then 
-				     								  	(matchRuleTerm ruleFunc termTail acceptor deriv fragTail )
+				     								  	(matchRuleTerm ruleFunc termTail acceptor fragTail deriv)
 				     								  else
 				     									None)
 				     
@@ -60,16 +60,16 @@ match arrowList with
 			| Some(x, y) -> Some (x, y)
 			(* we succeeded, return itself *)*)
 
-and matchRuleList start_symbol rule_list_func alter_list accept d frag = match alter_list with 
+and matchRuleList start_symbol rule_list_func alter_list accept frag deriv = match alter_list with 
 	| [] -> None
-	| h::t -> match (check_single_rule rule_list_func h accept (d@[start_symbol,h]) frag) with 
-		| None -> check_alternative_list start_symbol rule_list_func t accept d frag
+	| h::t -> match (check_single_rule rule_list_func h accept frag (deriv::[start_symbol,h])) with 
+		| None -> check_alternative_list start_symbol rule_list_func t accept frag deriv
 		| Some(a,b) -> Some(a,b)
 
 
 let  parse_prefix gram acceptor frag = 
 	match gram with
-	| (start, ruleFunc) -> matchRuleList start ruleFunc (ruleFunc start) acceptor [] frag 
+	| (start, ruleFunc) -> matchRuleList start ruleFunc (ruleFunc start) acceptor frag []
 
 
 
