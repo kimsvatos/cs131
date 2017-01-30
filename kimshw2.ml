cea526c2c5012368769c392ea5dd1251a0b0843f
kimsvatos <kimsvatos@ucla.edu>
2017-01-30 13:12:26 -0800

    syntax error fix

--- kimshw2.ml
+++ kimshw2.ml
@@ -67,7 +67,7 @@ let rec matchRuleTerm ruleSymbol ruleFunc acceptor frag deriv =
 				     								  else
 				     									None
 				     	(* ^^ terminal, so we can match the NEXT part of Frag*)
-						| (N nonTermHead)::(nonTermTail) -> matchRuleList ruleSymbol ruleFunc (matchRuleTerm nonTermTail ruleFunc acceptor frag deriv) frag deriv
+						| (N nonTermHead)::(nonTermTail) -> (matchRuleList ruleSymbol ruleFunc (matchRuleTerm nonTermTail ruleFunc acceptor frag deriv) frag deriv)
 						(*non terminal, must expand with match ruleList*)
 
 
