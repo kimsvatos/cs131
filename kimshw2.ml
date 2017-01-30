675a0e229aec511638856b2ea5e8ba948ff0efa1
kimsvatos <kimsvatos@ucla.edu>
2017-01-30 15:23:16 -0800

    missing parenth

--- kimshw2.ml
+++ kimshw2.ml
@@ -4,7 +4,7 @@ let rec createRules nonterminal gram1 =
  	match gram1 with
  	| [] -> []
  	| (left, right)::t -> if left = nonterminal then
- 	                     		[right]@(createRules left t)
+ 	                     		right::(createRules left t)
  	                      else
  	                      		createRules left t 
 
@@ -27,29 +27,29 @@ type ('nonterminal, 'terminal) symbol =
 
 
 
-let rec matchRuleTerm ruleFunc ruleSymbol acceptor frag deriv= 
+let rec matchRuleTerm ruleFunc ruleSymbol acceptor deriv frag= 
 	match ruleSymbol with
 	[] -> acceptor deriv frag
 	| _ -> match frag with 
 			| [] -> None
 			| (fragHead)::(fragTail) -> match ruleSymbol with 
 					| (N nonTermHead)::(nonTermTail) -> 
-					(matchRuleList (nonTermHead) (ruleFunc) (ruleFunc nonTermHead) (matchRuleTerm ruleFunc nonTermTail acceptor) frag deriv)
+					(matchRuleList (nonTermHead) (ruleFunc) (ruleFunc nonTermHead) (matchRuleTerm ruleFunc nonTermTail acceptor) deriv frag)
 				     | (T termHead)::(termTail) -> if fragHead = termHead then 
-				     								  (matchRuleTerm ruleFunc termTail acceptor fragTail deriv) else None
+				     								  (matchRuleTerm ruleFunc termTail acceptor deriv fragTail) else None
 
 
-and matchRuleList start ruleFunc arrowList accept frag deriv=
+and matchRuleList start ruleFunc arrowList accept deriv frag =
  match arrowList with 
 	| [] -> None
-	| h::t -> match (matchRuleTerm ruleFunc h accept frag (deriv@[start,h])) with 
-		| None -> matchRuleList start ruleFunc t accept frag deriv
+	| h::t -> match (matchRuleTerm ruleFunc h accept (deriv@[start,h]) frag) with 
+		| None -> matchRuleList start ruleFunc t accept deriv frag
 		| Some(a,b) -> Some(a,b)
 
 
 let  parse_prefix gram acceptor frag = 
 	match gram with
-	| (start, ruleFunc) -> matchRuleList start ruleFunc (ruleFunc start) acceptor frag []
+	| (start, ruleFunc) -> matchRuleList start ruleFunc (ruleFunc start) acceptor [] frag
 
 
 
