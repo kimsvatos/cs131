a303b87b2574343b91bb2e86d22958ed433fa8c1
kimsvatos <kimsvatos@ucla.edu>
2017-01-30 16:55:06 -0800

    trying to find good one -- trying the @ vs ::

--- kimshw2.ml
+++ kimshw2.ml
@@ -1,4 +1,5 @@
 
+
 let rec createRules nonterminal gram1 = 
  	match gram1 with
  	| [] -> []
@@ -13,7 +14,12 @@ let convert_grammar gram1 =
 	| (start, rules) -> (start, (createRules rules))
 
 
-(* from hw 1 type defs *)
+
+
+(* start list is a list of list [[blah; blah] ; [blah] ; [blah; blah; blah] ]  
+	rules func: term -> list of possibilities (tree)
+	frag: literal string we parse
+*)
 
 type ('nonterminal, 'terminal) symbol =
   | N of 'nonterminal
@@ -21,40 +27,24 @@ type ('nonterminal, 'terminal) symbol =
 
 
 
-let rec matchRuleTerm ruleSymbol ruleFunc acceptor deriv frag= 
-	(*match ruleSymbol with*)
-	(*[] -> acceptor deriv frag*) 
-	(* there are no rules left, check if acceptor is chill *)
-(*	| _ ->*)match frag with 
-			| [] -> None 
-			(* ^^ we are at the end of the fragment, but have rules left. Cant work. None*)
+let rec matchRuleTerm ruleFunc ruleSymbol acceptor deriv frag= 
+	match ruleSymbol with
+	[] -> acceptor deriv frag
+	| _ -> match frag with 
+			| [] -> None
 			| (fragHead)::(fragTail) -> match ruleSymbol with 
-					| [] -> acceptor deriv frag 
 					| (N nonTermHead)::(nonTermTail) -> 
-					(* if nonterminal, must expand this list how we did with Start, call matchRuleList*)
 					(matchRuleList (nonTermHead) (ruleFunc) (ruleFunc nonTermHead) (matchRuleTerm ruleFunc nonTermTail acceptor) deriv frag)
 				     | (T termHead)::(termTail) -> if fragHead = termHead then 
-				     								  (matchRuleTerm termTail ruleFunc acceptor deriv fragTail) else None
-				     (*if terminal, check that the fragment matches the term. If no, NONE try a diff one. 
-				     	if YES, we attempt to keep matching rules, excluding this section of the frag,
-				     	and moving on to the next term in the rules*)
+				     								  (matchRuleTerm ruleFunc termTail acceptor deriv fragTail) else None
 
 
-(* arrowList is what a nonTerminal leads to, eg
-Expr -> [term;binop;expr] ; [term]; [expr] 
-the entire right part of the arrow is arrowList, we must check the next one 
-if the first one fails and returns None
-*)
 and matchRuleList start ruleFunc arrowList accept deriv frag =
  match arrowList with 
 	| [] -> None
-	| h::t -> match (matchRuleTerm h ruleFunc accept (deriv@[start,h]) frag) with 
-	(* try this combination, adding this start and chunk of arrowList to the deriv*)
-		| Some(x,y) -> Some(x,y) 
-		(* We're done! otherwise, try a diff possiblity of ArrowList*)
+	| h::t -> match (matchRuleTerm ruleFunc h accept (deriv@[start,h]) frag) with 
 		| None -> matchRuleList start ruleFunc t accept deriv frag
-		(* this failed--dont include this in the deriv, try the next chunk of arrowList as an attempt*)
-		
+		| Some(a,b) -> Some(a,b)
 
 
 let  parse_prefix gram acceptor frag = 
