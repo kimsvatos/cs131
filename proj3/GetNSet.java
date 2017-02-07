840d565278db1b6379d75b6f37e5eaeddb7fc250
kimsvatos <kimsvatos@ucla.edu>
2017-02-06 21:35:03 -0800

    getnset BUG test

--- proj3/GetNSet.java
+++ proj3/GetNSet.java
@@ -1,78 +1,46 @@
 import java.util.concurrent.atomic.AtomicIntegerArray;
 
-class GetNSet implements State {
-    private byte[] value;
+class GetNSet implements State{
+
     private byte maxval;
+    private AtomicIntegerArray m_value;
+    
+    GetNSet(byte[] val) {
 
- // matches size and elements of value byte array
-    private AtomicIntegerArray aValue; 
-
-    GetNSet(byte[] v) {
-    System.out.println("getnset initial\n"); 
-        value = v; 
-        int[] holderArr = new int[v.length];
-        int i = 0;
-        while(i < v.length){
-            holderArr[i] = value[i];
-            i++;
-        }
-
+        int[] vl = new int[val.length];
+        for (int i = 0; i < val.length; i++)
+            vl[i] = val[i];
+        m_value = new AtomicIntegerArray(vl); 
         maxval = 127; 
-        aValue = new AtomicIntegerArray(holderArr);
-        System.out.println("getnset initial END 1\n");
     }
 
+    GetNSet(byte[] val, byte m) { 
 
-    GetNSet(byte[] v, byte m) { 
-        System.out.println("getnset initial 2\n");
-        value = v; 
-        int[] holderArr = new int[v.length];
-        int i = 0;
-        while(i < v.length){
-            holderArr[i] = value[i];
-            i++;
-        }
-
+        int[] vl = new int[val.length];
+        for (int i = 0; i < val.length; i++)
+            vl[i] = val[i];
+        m_value = new AtomicIntegerArray(vl); 
         maxval = m; 
-        aValue = new AtomicIntegerArray(holderArr);
-        System.out.println("getnset initial END 2\n");
     }
-
-
-    public int size() { return aValue.length(); }
+    
+    public int size() { return m_value.length(); }
 
     public byte[] current() { 
-        System.out.println("start current\n");
-        value = new byte[aValue.length()];
-        int i = 0; 
-        while (i < aValue.length()){
-            value[i] = (byte) aValue.get(i);
-            i++;
-        }
-        System.out.println("end current\n");
-        return value;
 
-     }
-
-    
-    //public int jVal;
+        byte[] byte_array = new byte[m_value.length()];
+        for (int i = 0; i < m_value.length(); i++)
+            byte_array[i] = (byte) m_value.get(i);
+        return byte_array;
+    }
 
     public boolean swap(int i, int j) {
 
-    //int iVal = aValue.get(i);
-    //int jVal = aValue.get(j);
-    //System.out.println("swap " + iVal + " " + jVal  + "\n");
-	if (aValue.get(i) <= 0 || aValue.get(j) >= maxval) {
-	    return false;
-	}
+        if (m_value.get(i) <= 0 || m_value.get(j) >= maxval)
+            return false;
 
-   // public int iVal = aValue.get(i);
-    aValue.set(i, aValue.get(i)-1);
-    //public int jVal = aValue.get(j);
-    aValue.set(j, aValue.get(j)+1);
-
-	//value[i]--;
-	//value[j]++;
-	return true;
+        m_value.set(i, m_value.get(i)-1);
+        m_value.set(j, m_value.get(j)+1);
+        
+        return true;
     }
 }
