cca233b8fa15e8ee18a661e1344aee0297ed5020
kimsvatos <kimsvatos@ucla.edu>
2017-02-06 21:37:35 -0800

    getnset BUG test

--- proj3/GetNSet.java
+++ proj3/GetNSet.java
@@ -1,46 +1,41 @@
 import java.util.concurrent.atomic.AtomicIntegerArray;
 
-class GetNSet implements State{
-
+class GetNSetState implements State {
+    private AtomicIntegerArray value;
     private byte maxval;
-    private AtomicIntegerArray m_value;
-    
-    GetNSet(byte[] val) {
 
-        int[] vl = new int[val.length];
-        for (int i = 0; i < val.length; i++)
-            vl[i] = val[i];
-        m_value = new AtomicIntegerArray(vl); 
+    GetNSetState(byte[] v) { 
+        value = new AtomicIntegerArray(v.length);
+        for (int i = 0; i < v.length; i++) {
+            value.set(i, v[i]);
+        }
         maxval = 127; 
     }
 
-    GetNSet(byte[] val, byte m) { 
-
-        int[] vl = new int[val.length];
-        for (int i = 0; i < val.length; i++)
-            vl[i] = val[i];
-        m_value = new AtomicIntegerArray(vl); 
+    GetNSetState(byte[] v, byte m) { 
+        value = new AtomicIntegerArray(v.length);
+        for (int i = 0; i < v.length; i++) {
+            value.set(i, v[i]);
+        }
         maxval = m; 
     }
-    
-    public int size() { return m_value.length(); }
+
+    public int size() { return value.length(); }
 
     public byte[] current() { 
-
-        byte[] byte_array = new byte[m_value.length()];
-        for (int i = 0; i < m_value.length(); i++)
-            byte_array[i] = (byte) m_value.get(i);
-        return byte_array;
+        byte[] temp = new byte[value.length()];
+        for (int i = 0; i < value.length(); i++) {
+            temp[i] = (byte) value.get(i);
+        }
+        return temp;
     }
 
     public boolean swap(int i, int j) {
-
-        if (m_value.get(i) <= 0 || m_value.get(j) >= maxval)
+        if (value.get(i) <= 0 || value.get(j) >= maxval) {
             return false;
-
-        m_value.set(i, m_value.get(i)-1);
-        m_value.set(j, m_value.get(j)+1);
-        
+        }
+        value.getAndDecrement(i);
+        value.getAndIncrement(j);
         return true;
     }
 }
