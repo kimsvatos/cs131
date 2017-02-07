7fc89eaa25ef1698628d1388842d482f0bc86c13
kimsvatos <kimsvatos@ucla.edu>
2017-02-06 21:27:49 -0800

    getnset seems endless

--- proj3/GetNSet.java
+++ proj3/GetNSet.java
@@ -55,8 +55,8 @@ class GetNSet implements State {
 
     public boolean swap(int i, int j) {
 
-    public int iVal; = aValue.get(i);
-    public int jVal; = aValue.get(j);
+    public int iVal = aValue.get(i);
+    public int jVal = aValue.get(j);
     //System.out.println("swap " + iVal + " " + jVal  + "\n");
 	if (iVal <= 0 || jVal >= maxval) {
 	    return false;
