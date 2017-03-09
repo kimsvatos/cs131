49379f0a3bce524c02efea8eb15d2bfb0350791c
kimsvatos <kimsvatos@ucla.edu>
2017-03-09 13:42:47 -0800

    make it listen to a port based on arg

--- server.py
+++ server.py
@@ -28,6 +28,9 @@ PORT_NUM = {
 
 # This is just about the simplest possible protocol
 class Echo(Protocol):
+	def connectionMade(self):
+		self.transport.write("connection made\r\n")
+
     def dataReceived(self, data):
         """
         As soon as any data is received, write it back.
@@ -40,15 +43,57 @@ def main():
 	for arg in sys.argv:
 		if arg == "server.py":
 			continue 
+	##########
+	# ALFORD below
+	##########
 		elif arg == str(PORT_NUM['ALFORD']):
 			print arg 
+			f = Factory()
+			f.protocol = Echo
+			reactor.listenTCP((PORT_NUM['ALFORD']), f)
+			reactor.run()
+	##########
+	# BALL below
+	##########
+		elif arg == str(PORT_NUM['BALL']):
+			print arg 
+			f = Factory()
+			f.protocol = Echo
+			reactor.listenTCP((PORT_NUM['BALL']), f)
+			reactor.run()	##########
+	# HAMILTON below
+	##########
+		elif arg == str(PORT_NUM['HAMILTON']):
+			print arg 
+			f = Factory()
+			f.protocol = Echo
+			reactor.listenTCP((PORT_NUM['HAMILTON']), f)
+			reactor.run()
+	##########
+	# HOLIDAY below
+	##########
+		elif arg == str(PORT_NUM['HOLIDAY']):
+			print arg 
+			f = Factory()
+			f.protocol = Echo
+			reactor.listenTCP((PORT_NUM['HOLDAY']), f)
+			reactor.run()
+	##########
+	# WELSH below
+	##########
+		elif arg == str(PORT_NUM['WELSH']):
+			print arg 
+			f = Factory()
+			f.protocol = Echo
+			reactor.listenTCP((PORT_NUM['WELSH']), f)
+			reactor.run()
 		else:
 			print "boo"
 
-	f = Factory()
-	f.protocol = Echo
-	reactor.listenTCP(8000, f)
-	reactor.run()
+	#f = Factory()
+	#f.protocol = Echo
+	#reactor.listenTCP(8000, f)
+	#reactor.run()
 
 if __name__ == '__main__':
     main()
