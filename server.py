932a7a16e086c2caf40f1df74fc21dbdee86e3e0
kimsvatos <kimsvatos@ucla.edu>
2017-03-11 13:35:49 -0800

    json being nothing?

--- server.py
+++ server.py
@@ -233,7 +233,7 @@ class Server(LineReceiver):
 		loc = loc.replace("+", " +")
 		loc = loc.split()
 		
-		
+		print("about to request google")
 		url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={0},{1}&radius={2}&types=food&name=cruise&key={3}".format(loc[0], loc[1], rad, conf.API_KEY)
 		pageGot = getPage(url)
 		pageGot.addCallback(self.handle_JSON, pageGot, limit=limit, client=client)
